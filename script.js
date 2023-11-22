f6fe3f928458fabde743bd8cbf8e02943a85f944
yehlingsss <137746308+yehlingsss@users.noreply.github.com>
2023-11-22 23:55:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function populateDropdown(category, placeholder) {
     defaultOption.textContent = placeholder;
     dropdown.appendChild(defaultOption);
 
-    const selectedCategory = document.getElementById(`${category.toLowerCase()}Result`);
+    const selectedCategory = document.getElementById(`${category}Result`);
     if (selectedCategory) {
         const locations = selectedCategory.querySelectorAll('ul');
         locations.forEach(location => {
-            const locationName = location.id.replace(`${category.toLowerCase()}`, '');
+            const locationName = location.id.replace(`${category}`, '');
             const capitalizedLocation = locationName.charAt(0).toUpperCase() + locationName.slice(1).toLowerCase();
             const option = document.createElement('option');
             option.value = locationName.toLowerCase();
@@ -34,41 +34,38 @@ function showDropdown(category, placeholder) {
 }
 
 function showListings(location) {
-    const allLists = document.querySelectorAll('.searchResultListing > div');
+    const allLists = document.querySelectorAll('#searchResults > div');
     allLists.forEach(list => {
         list.style.display = 'none';
     });
 
+    function getCategory() {
+        const suppliesResults = document.getElementById('suppliesResult');
+        const groomersResults = document.getElementById('groomersResult');
+        const vetsResults = document.getElementById('vetsResult');
+        const boardingResults = document.getElementById('boardingResult');
+        const othersResults = document.getElementById('othersResult');
+    
+        if (suppliesResults.style.display === 'block') {
+            return 'Supplies';
+        } else if (groomersResults.style.display === 'block') {
+            return 'Groomers';
+        } else if (vetsResults.style.display === 'block') {
+            return 'Vets';
+        } else if (boardingResults.style.display === 'block') {
+            return 'Boarding';
+        } else if (othersResults.style.display === 'block') {
+            return 'Others';
+        }
+    }
+
     const category = getCategory();
-    console.log('Category:', category);
     
     const specificList = document.getElementById(`${category.toLowerCase()}Result`);
-    console.log('Specific List:', specificList);
 
-    const specificLocationList = specificList.querySelector(`#${category.toLowerCase()}${location}`);
-    console.log('Specific Location List:', specificLocationList);
+    const specificLocationList = specificList.querySelector(`#${category}${location}`);
     
     if (specificLocationList) {
         specificLocationList.style.display = 'block';
-    }
-}
-
-function getCategory() {
-    const suppliesResults = document.getElementById('suppliesResult');
-    const groomersResults = document.getElementById('groomersResult');
-    const vetsResults = document.getElementById('vetsResult');
-    const boardingResults = document.getElementById('boardingResult');
-    const othersResults = document.getElementById('othersResult');
-
-    if (suppliesResults.style.display === 'block') {
-        return 'Supplies';
-    } else if (groomersResults.style.display === 'block') {
-        return 'Groomers';
-    } else if (vetsResults.style.display === 'block') {
-        return 'Vets';
-    } else if (boardingResults.style.display === 'block') {
-        return 'Boarding';
-    } else if (othersResults.style.display === 'block') {
-        return 'Others';
     }
 }
